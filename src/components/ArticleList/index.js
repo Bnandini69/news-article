8d2f5216c617cb52300060998e393c81cf41d05e
Bhairivi <bhairivi@ideaclan.com>
2024-08-15 17:43:40 +0530

    update: article list

---
+++ src/components/ArticleList/index.js
@@ -0,0 +1,88 @@
+import React from "react";
+
+const ArticleList = () => {
+  const { data} = useSelector((state) => state.news);
+
+  return (
+    <>
+      {data.length > 0 ? (
+        <div>
+          <div
+            style={{
+              position: "fixed",
+              top: 0,
+              left: 0,
+              right: 0,
+              background: "white",
+              zIndex: 1000,
+              padding: "10px",
+              boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+            }}
+          >
+            <FilterComponent />
+          </div>
+          <div style={{ marginTop: "20px" }}>
+            <Row gutter={16}>
+              {data
+                .filter((article) => article.title && article.description)
+                .map((article, index) => (
+                  <Col
+                    xs={24}
+                    sm={12}
+                    md={8}
+                    lg={6}
+                    key={article.url || index}
+                    style={{ marginBottom: "16px", padding: "0 8px" }}
+                  >
+                    <Card
+                      title={article.title}
+                      hoverable
+                      cover={
+                        <img
+                          alt={article.title}
+                          src={article.urlToImage}
+                          style={{
+                            width: "100%",
+                            height: "auto",
+                            maxHeight: "150px",
+                            objectFit: "cover",
+                          }}
+                        />
+                      }
+                      style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        height: "100%",
+                      }} // Set card to fill its container
+                    >
+                      <Meta
+                        description={article.description}
+                        style={{
+                          flex: 1,
+                          display: "flex",
+                          flexDirection: "column",
+                          justifyContent: "space-between",
+                        }}
+                      />
+                      <a
+                        href={article.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        style={{ marginTop: "8px", textAlign: "center" }}
+                      >
+                        Read more
+                      </a>
+                    </Card>
+                  </Col>
+                ))}
+            </Row>
+          </div>
+        </div>
+      ) : (
+        "No articles found"
+      )}
+    </>
+  );
+};
+
+export default ArticleList;
--- src/components/NewsFeed/index.js
+++ src/components/NewsFeed/index.js
@@ -10,6 +10,7 @@ import {
 import FilterComponent from "../Header";
 import debounce from "lodash.debounce"; // Add lodash.debounce package
 import Meta from "antd/es/card/Meta";
+import ArticleList from "../ArticleList";
 
 const API_KEY = "157fb9d58adc4b34bbb05eb4da5c76ef";
 const BASE_URL = "https://newsapi.org/v2/everything";
@@ -87,80 +88,7 @@ const NewsFeed = () => {
         />
       ) : (
         isChecked &&
-        data.length > 0 && (
-          <div>
-            <div
-              style={{
-                position: "fixed",
-                top: 0,
-                left: 0,
-                right: 0,
-                background: "white",
-                zIndex: 1000,
-                padding: "10px",
-                boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
-              }}
-            >
-              <FilterComponent />
-            </div>
-            <div style={{ marginTop: "20px" }}>
-              <Row gutter={16}>
-                {data
-                  .filter((article) => article.title && article.description)
-                  .map((article, index) => (
-                    <Col
-                      xs={24}
-                      sm={12}
-                      md={8}
-                      lg={6}
-                      key={article.url || index}
-                      style={{ marginBottom: "16px", padding: "0 8px" }}
-                    >
-                      <Card
-                        title={article.title}
-                        hoverable
-                        cover={
-                          <img
-                            alt={article.title}
-                            src={article.urlToImage}
-                            style={{
-                              width: "100%",
-                              height: "auto",
-                              maxHeight: "150px",
-                              objectFit: "cover",
-                            }}
-                          />
-                        }
-                        style={{
-                          display: "flex",
-                          flexDirection: "column",
-                          height: "100%",
-                        }} // Set card to fill its container
-                      >
-                        <Meta
-                          description={article.description}
-                          style={{
-                            flex: 1,
-                            display: "flex",
-                            flexDirection: "column",
-                            justifyContent: "space-between",
-                          }}
-                        />
-                        <a
-                          href={article.url}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          style={{ marginTop: "8px", textAlign: "center" }}
-                        >
-                          Read more
-                        </a>
-                      </Card>
-                    </Col>
-                  ))}
-              </Row>
-            </div>
-          </div>
-        )
+       <ArticleList />
       )}
     </div>
   );
